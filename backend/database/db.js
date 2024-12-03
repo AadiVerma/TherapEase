c50388553ac18eedc3ffb397d34d6fd549a257df
Aditya Verma <adityakaplish11@gmail.com>
2024-12-03 15:39:16 +0530

    main

---
+++ backend/database/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+
+export const Connection = async () => {
+    const URL = process.env.MOGODB_URL;
+
+    try {
+        await mongoose.connect(`mongodb+srv://harshal1629be22:Bhardwaj!12!3@cluster0.zifcw6q.mongodb.net/harshal`, {useUnifiedTopology: true, useNewUrlParser: true});
+        console.log ('Database connected successfully!!!');
+    } catch (error) {
+        console.log ('Error while connecting with the database', error.message);
+    }
+}
+
+export default Connection;
